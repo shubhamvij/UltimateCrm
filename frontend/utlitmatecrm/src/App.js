547db38a5c4f58a99783be1d2c2870be7f9460d5
Shubham Vij <reachme@shubhamvij.com>
2017-06-25 16:27:54 -0400

    Added materialui css components

--- frontend/utlitmatecrm/src/App.js
+++ frontend/utlitmatecrm/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './material.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="app">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
@@ -13,6 +14,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <button className="mdc-button">
+            Flat button
+      </button>
       </div>
     );
   }
