4f19fd839ab9b2b40251f399b611e0e95a94233a
Tian Hu <tianhu@Tians-MacBook-Pro-2.local>
2017-06-28 23:02:56 -0400

    Added API data to issues

--- frontend/utlitmatecrm/src/Views/IssueView/IssueView.js
+++ frontend/utlitmatecrm/src/Views/IssueView/IssueView.js
@@ -12,10 +12,35 @@ const testData = [
     {id: '4', title: 'Test 4'},
     {id: '5', title: 'Test 5'},
   ]
-const listItems = testData.map((item) => <li key={item.id} className="mdc-list-item"><i className="material-icons mdc-list-item__start-detail">
-                                bluetooth
-                              </i>{item.title}</li>);
 class IssueView extends Component {
+  constructor(){
+    super();
+    this.state = {issues:[], filter:'all'};
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:3000/api/issues')
+        .then(result => result.json())
+        .then(issues => this.setState({issues}));
+
+    this.closeIssue = this.closeIssue.bind(this);
+    this.filterItem = this.filterItem.bind(this);
+  }
+
+  closeIssue() {
+     this.setState({newCustomer: false});
+  }
+
+  filterItem(item) {
+    if (this.state.filter === 'all'){
+      return true;
+    } else if (this.state.filter === 'open_me'){
+      return item.opened_by.id == this.props.activeCustomerId? this.props.activeCustomerId: item.opened_by.id; 
+    } else if (this.state.filter === 'ass_me'){
+      return item.assigned_to.id == this.props.activeCustomerId? this.props.activeCustomerId: item.assigned_to.id;
+    }
+  }
+
   render() {
     return (
         <div>
@@ -24,13 +49,13 @@ class IssueView extends Component {
                     <div className="mdc-layout-grid__cell mdc-layout-grid__cell--span-3">
                         <nav className="mdc-permanent-drawer mdc-typography">
                           <nav id="icon-with-text-demo" className="mdc-list">
-                            <a className="mdc-list-item mdc-permanent-drawer--selected" href="#">
+                            <a className="mdc-list-item mdc-permanent-drawer--selected" href="#" onClick={() => this.setState({filter: 'all'})}>
                               <i className="material-icons mdc-list-item__start-detail" aria-hidden="true">inbox</i>All
                             </a>
-                            <a className="mdc-list-item" href="#">
-                              <i className="material-icons mdc-list-item__start-detail" aria-hidden="true">star</i>Favourites
+                            <a className="mdc-list-item" href="#" onClick={() => this.setState({filter: 'open_me'})}>
+                              <i className="material-icons mdc-list-item__start-detail" aria-hidden="true">star</i>Opened by me
                             </a>
-                            <a className="mdc-list-item" href="#">
+                            <a className="mdc-list-item" href="#" onClick={() => this.setState({filter: 'ass_me'})}>
                               <i className="material-icons mdc-list-item__start-detail" aria-hidden="true">star</i>Assigned to me
                             </a>
                           </nav>
@@ -38,9 +63,21 @@ class IssueView extends Component {
                     </div>
                     <div className="mdc-layout-grid__cell mdc-layout-grid__cell--span-9">
                       <div className="mdc-elevation--z2">
-                          <ul className="mdc-list">
-                            {listItems}
-                          </ul>
+                          {this.state.issues.filter(this.filterItem).map((item) => <div className="mdc-card">
+                            <section className="mdc-card__primary">
+                              <h1 className="mdc-card__title mdc-card__title--large">{item.title}<br/></h1>
+                              <h2 className="mdc-card__subtitle">Assigned to: {item.assigned_to.first_name} {item.assigned_to.last_name}
+                                  <br/>Opened by: {item.opened_by.first_name} {item.opened_by.last_name}
+                              </h2>
+                            </section>
+                            <section className="mdc-card__supporting-text">
+                              <p><h3>Issue: {item.issue_type.name}</h3>{item.issue_type.description}</p>
+                              <p><h3>Notes:</h3>{item.issue_notes}</p>
+                            </section>
+                            <section className="mdc-card__actions">
+                              <button className="mdc-button mdc-button--compact mdc-card__action">Close</button>
+                            </section>
+                          </div>)}
                       </div>
                       <button className="mdc-fab material-icons add-button-right" aria-label="Add">
                             <span className="mdc-fab__icon">
